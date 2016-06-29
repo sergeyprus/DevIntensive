1ee4cc02fac8f9fe9cf4acce99625f9e908a7f62
sergeyprus <ps-am@mail.ru>
2016-06-29 09:26:38 +0300

    Add files via upload

---
+++ ConstantManager.java
@@ -0,0 +1,17 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Created by Person on 23.06.16.
+ */
+public interface ConstantManager {
+    String TAG_PREFIX="DEV";
+
+    String EDIT_MODE_KEY = "EDIT_MODE_KEY";
+
+    String USER_PHONE_KEY = "USER_PHONE_KEY" ;
+    String USER_EMAIL_KEY = "USER_EMAIL_KEY";
+    String USER_VK_KEY = "USER_VK_KEY";
+    String USER_GIT_KEY = "USER_GIT_KEY";
+    String USER_ABOUT_KEY = "USER_ABOUT_KEY";
+
+}
---
+++ DataManager.java
@@ -0,0 +1,23 @@
+package com.softdesign.devintensive.data.managers;
+
+
+public class DataManager {
+    private static DataManager INSTANCE = null;
+
+    private PreferencesManager mPreferencesManager;
+
+    public DataManager() {
+        this.mPreferencesManager = new PreferencesManager();
+    }
+
+    public static DataManager getInstanse(){
+        if (INSTANCE==null){
+            INSTANCE = new DataManager();
+        }
+        return INSTANCE;
+    }
+
+    public PreferencesManager getPreferencesManager() {
+        return mPreferencesManager;
+    }
+}
